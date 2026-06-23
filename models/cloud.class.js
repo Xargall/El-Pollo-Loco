cd957e459d442dcdb8a2cd199c85c34a2b6e1f61
Mathias Mayer <mayer.mathias@proton.me>
2026-06-23 12:10:50 +0200

    add: cloud movement to the left @ 60 FPS

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,18 @@
 class Cloud extends MovableObject {
     y = 20;
+    width = 500;
+    height = 250;
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png')
         this.x = 0 + Math.random() * 500; // Random x position between 200 and 700
-        this.width = 500;
-        this.height = 250;
+        this.animate();
 
+    }
+
+    // Animate the cloud's movement to the left
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60)
     }
 }
