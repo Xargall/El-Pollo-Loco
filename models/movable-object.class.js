c8966972e34bc36c715e8d837189e4adeda4b3d8
Mathias Mayer <mayer.mathias@proton.me>
2026-06-23 08:38:14 +0200

    add: draw character and enemies on canvas, add requestAnimationFrame(),

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,15 @@
 class MovableObject {
   x = 120;
-  y = 400;
+  y = 250;
   img;
+  height = 150;
+  width = 100;
+
+  // Load an image from the specified path
+  loadImage(path) {
+    this.img = new Image(); // Create a new image object
+    this.img.src = path; // Set the image source
+  }
 
   moveRight() {
     console.log("move right");
--- models/world.class.js
+++ models/world.class.js
@@ -1,5 +1,34 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-  draw() {}
+  canvas;
+  ctx;
+
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.draw();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // Clear the canvas for the next frame
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height,
+    );
+    // Draw all enemies on the canvas
+    this.enemies.forEach((enemy) => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height,
+      );
+    });
+    requestAnimationFrame(() => this.draw()); // Call draw for the next frame, defined by Graphics Card
+  }
 }
