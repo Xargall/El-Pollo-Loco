c17aceeb4df42e61dd09abf0bef4cb51a0296461
Mathias Mayer <mayer.mathias@proton.me>
2026-06-24 08:12:03 +0200

    add: eventListener for keyboard input

--- models/character.class.js
+++ models/character.class.js
@@ -14,6 +14,15 @@ class Character extends MovableObject {
     "img/2_character_pepe/1_idle/idle/I-10.png",
   ];
 
+  IMAGES_WALKING = ["img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",];
+
+  world;
+
 
 
   constructor() {
@@ -25,11 +34,19 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
+      if (this.world.keyboard.RIGHT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+
       let i = this.currentImage % this.IMAGES_IDLE.length;
       let path = this.IMAGES_IDLE[i];
       this.img = this.imageCache[path];
       this.currentImage++;
     }, 100)
+
 
 
   }
--- models/world.class.js
+++ models/world.class.js
@@ -1,7 +1,10 @@
 class World {
   character = new Character();
+
   enemies = [new Chicken(), new Chicken(), new Chicken()];
+
   clouds = [new Cloud()];
+
   backgroundObjects = [
     new BackgroundObject("img/5_background/layers/air.png", 0),
     new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
@@ -10,11 +13,18 @@ class World {
   ];
   canvas;
   ctx;
+  keyboard;
 
-  constructor(canvas) {
+  constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
+    this.keyboard = keyboard
     this.draw();
+    this.setWorld();
+  }
+
+  setWorld() {
+    this.character.world = this;
   }
 
   draw() {
