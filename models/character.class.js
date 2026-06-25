ca88b8a3a27234959280505db837b93f1d375a1e
Mathias Mayer <mayer.mathias@proton.me>
2026-06-25 14:12:27 +0200

    fix: animation timing on idle, long idle and jumping. created seperate intervalls with unique if-statements

--- models/character.class.js
+++ models/character.class.js
@@ -106,6 +106,7 @@ class Character extends MovableObject {
 
       if (this.world.keyboard.SPACE && !this.isAboveGround()) {
         this.jump();
+        this.idleStart = new Date().getTime();
       }
 
       this.world.camera_x = -this.x + 100;
